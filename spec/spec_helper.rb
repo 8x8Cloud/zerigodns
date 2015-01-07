a178f883f83612fa82ea1339980e4fc012659df7
Raymond F. Wells <rfw2nd@gmail.com>
2015-01-07 13:31:36 -0800

    Change zerigo_dns -> zerigodns to follow convention.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@
 # require 'spec/interop/test'
 require 'simplecov'
 require 'logger'
-require File.expand_path(File.dirname(__FILE__) + "/../lib/zerigo_dns")
+require File.expand_path(File.dirname(__FILE__) + "/../lib/zerigodns")
 require_relative 'features/feature_spec_helper'
 
 
