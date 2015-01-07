950f2a343e23dd82ae0e874eafa42283e27708af
Raymond F. Wells <rfw2nd@gmail.com>
2015-01-06 21:40:27 -0800

    Add documentation

--- lib/zerigo/dns/tools.rb
+++ lib/zerigo/dns/tools.rb
@@ -2,10 +2,15 @@ require_relative 'base'
 
 class Zerigo::DNS::Tools < Zerigo::DNS::Base
   class <<self
+    
+    # Fetch current public ipv4 address
+    # @return [String] Current public ipv4 address or "unknown"
     def public_ipv4
       get :public_ipv4
     end
     
+    # Fetch current public ipv6 address
+    # @return [String] Current public ipv6 address or "unknown"
     def public_ipv6
       get :public_ipv6
     end
