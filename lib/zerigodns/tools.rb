5c98245248ce1a7af76fc245d012d8567dd00fa0
Raymond F. Wells <rfw2nd@gmail.com>
2015-02-03 13:49:49 -0800

    We don't need gyoku

--- lib/zerigodns/tools.rb
+++ lib/zerigodns/tools.rb
@@ -4,19 +4,19 @@ class ZerigoDNS::Tools < ZerigoDNS::Client
     # Fetch current public ipv4 address
     # @return [String] Current public ipv4 address or "unknown"
     def public_ipv4
-      get '/tools/public_ipv4.xml'
+      get('tools/public_ipv4.xml')
     end
     
     # Fetch current public ipv6 address
     # @return [String] Current public ipv6 address or "unknown"
     def public_ipv6
-      get '/tools/public_ipv6.xml'
+      get('tools/public_ipv6.xml')
     end
     
     # Fetch the current public IP address (either ipv4 or ipv6)
     # @return [String] Current public ip address (ipv4 or ipv6)
     def public_ip
-      get '/tools/public_ip'
+      get('tools/public_ip')
     end
   end
 
--- zerigodns.gemspec
+++ zerigodns.gemspec
@@ -24,7 +24,6 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'faraday', '~> 0.9.1'
   s.add_dependency 'multi_xml', '~> 0.5.5'
-  s.add_dependency 'gyoku', '~> 1.2.2'
 
   s.add_development_dependency 'yard', '~> 0.8.7.6'  
   s.add_development_dependency 'rspec', '~> 2.99'
